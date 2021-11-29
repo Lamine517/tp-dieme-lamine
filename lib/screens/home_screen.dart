b955242d2fc6e344fa62fda3de7f10ba0c3aacc2
Lamine517 <diemelamine398@gmail.com>
2021-11-28 19:26:03 -0800

    mettre a jours les donness

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,6 +9,7 @@ import 'package:todoapp/models/task.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
+import 'package:todoapp/screens/task_screen.dart';
 
 Future<dynamic> _fetchTasks() {
   return http.get(tasksListURL);
@@ -41,7 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     var tasks = <TaskItem>[];
-    
+
     return AnimatedPositioned(
       top: 0,
       bottom: 0,
@@ -53,7 +54,10 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Scaffold(
           backgroundColor: Colors.transparent,
           floatingActionButton: FloatingActionButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => TaskScreen()));
+            },
             backgroundColor: businessIndicator,
             child: Icon(
               Icons.add,
@@ -111,137 +115,139 @@ class _HomeScreenState extends State<HomeScreen> {
                   height: 10,
                 ),
                 FutureBuilder(
-                  future: _fetchTasks(),
+                    future: _fetchTasks(),
                     builder: (BuildContext context, AsyncSnapshot snapShot) {
-                  if (snapShot.hasData) {
-                    if (snapShot.data.statusCode == 200) {
-                      for (var item
-                            in convert.jsonDecode(snapShot.data.body)) {
-                          tasks.add(TaskItem(task: Task.fromJson(item)));
-                        }
-                      return Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 25),
-                            child: Text(
-                              "What's up, Olivia!",
-                              style: TextStyle(
-                                  fontFamily: boldFont,
-                                  fontSize: 40,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 25),
-                            child: Text(
-                              "CATEGORIES",
-                              style: TextStyle(
-                                color: thirdFontColor.withOpacity(0.6),
-                                fontFamily: regularFont,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                              vertical: 10,
-                            ),
-                            height: 150,
-                            child: ListView(
-                              scrollDirection: Axis.horizontal,
-                              children: [
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                SliderItem(
-                                  category: "Business",
-                                  tasksCount: 40,
-                                  indicatorColor: businessIndicator,
-                                  screenWidth: widget.screenWidth,
-                                  terminationPercentage: .6,
-                                ),
-                                SliderItem(
-                                  category: "Personal",
-                                  tasksCount: 18,
-                                  indicatorColor: personalIndicator,
-                                  screenWidth: widget.screenWidth,
-                                  terminationPercentage: .4,
-                                )
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 25),
-                            child: Text(
-                              "TASKS",
-                              style: TextStyle(
-                                color: thirdFontColor.withOpacity(0.6),
-                                fontFamily: regularFont,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(horizontal: 25),
-                            height: .45 * widget.screenHeight,
-                            child: ListView(
-                              padding: EdgeInsets.all(0),
-                              children: tasks,
-                            ),
-                          ),
-                        ],
-                      );
-                    } else {
-                      return Center(
-                        child: Text('Request failed with status: ${snapShot.data.statusCode}.',),
-                      );
-                    }
-                  } else if (snapShot.hasError) {
-                      return SizedBox(
-                        height: widget.screenHeight - 140,
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                      if (snapShot.hasData) {
+                        if (snapShot.data.statusCode == 200) {
+                          for (var item
+                              in convert.jsonDecode(snapShot.data.body)) {
+                            tasks.add(TaskItem(task: Task.fromJson(item)));
+                          }
+                          return Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Container(
-                                margin: EdgeInsets.only(top: 10.0),
-                                height: 80.0,
-                                child: Image.asset(
-                                  "assets/images/no-internet.png",
-                                  color: Colors.white.withOpacity(.3),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 25),
+                                child: Text(
+                                  "What's up, Olivia!",
+                                  style: TextStyle(
+                                      fontFamily: boldFont,
+                                      fontSize: 40,
+                                      color: Colors.white),
                                 ),
                               ),
-                              Text(
-                                "Problème de connexion",
-                                style: TextStyle(
-                                  color: Colors.white.withOpacity(.3),
-                                  fontFamily: regularFont,
+                              SizedBox(
+                                height: 30,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 25),
+                                child: Text(
+                                  "CATEGORIES",
+                                  style: TextStyle(
+                                    color: thirdFontColor.withOpacity(0.6),
+                                    fontFamily: regularFont,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(
+                                  vertical: 10,
+                                ),
+                                height: 150,
+                                child: ListView(
+                                  scrollDirection: Axis.horizontal,
+                                  children: [
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    SliderItem(
+                                      category: "Business",
+                                      tasksCount: 40,
+                                      indicatorColor: businessIndicator,
+                                      screenWidth: widget.screenWidth,
+                                      terminationPercentage: .6,
+                                    ),
+                                    SliderItem(
+                                      category: "Personal",
+                                      tasksCount: 18,
+                                      indicatorColor: personalIndicator,
+                                      screenWidth: widget.screenWidth,
+                                      terminationPercentage: .4,
+                                    )
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 25),
+                                child: Text(
+                                  "TASKS",
+                                  style: TextStyle(
+                                    color: thirdFontColor.withOpacity(0.6),
+                                    fontFamily: regularFont,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Container(
+                                padding: EdgeInsets.symmetric(horizontal: 25),
+                                height: .45 * widget.screenHeight,
+                                child: ListView(
+                                  padding: EdgeInsets.all(0),
+                                  children: tasks,
                                 ),
                               ),
                             ],
+                          );
+                        } else {
+                          return Center(
+                            child: Text(
+                              'Request failed with status: ${snapShot.data.statusCode}.',
+                            ),
+                          );
+                        }
+                      } else if (snapShot.hasError) {
+                        return SizedBox(
+                          height: widget.screenHeight - 140,
+                          child: Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.only(top: 10.0),
+                                  height: 80.0,
+                                  child: Image.asset(
+                                    "assets/images/no-internet.png",
+                                    color: Colors.white.withOpacity(.3),
+                                  ),
+                                ),
+                                Text(
+                                  "Problème de connexion",
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(.3),
+                                    fontFamily: regularFont,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }
+                      return SizedBox(
+                        height: widget.screenHeight - 140,
+                        child: Center(
+                          child: CircularProgressIndicator(
+                            color: primaryBackground,
                           ),
                         ),
                       );
-                    }
-                  return SizedBox(
-                    height: widget.screenHeight - 140,
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        color: primaryBackground,
-                      ),
-                    ),
-                  );
-                }),
+                    }),
               ],
             ),
           ),
